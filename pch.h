2ae1198506e8eacc3dcd04659cbc2f7f44413807
Mouhamed Diallo <Mouhamed.dia2l@yahoo.fr>
2019-06-19 11:53:05 +0200

    Initialisation du projet

---
+++ pch.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifndef PCH_H
+#define PCH_H
+
+#include <stdio.h>
+#include <cstdlib>
+#include <tchar.h>
+#include <string>
+#include <sstream>
+#include <memory>
+#include <vector>
+#include <algorithm>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/OpenGL.hpp>
+
+#pragma comment(lib, "sfml-audio-d.lib")
+#pragma comment(lib, "sfml-graphics-d.lib")
+#pragma comment(lib, "sfml-main-d.lib")
+#pragma comment(lib, "sfml-system-d.lib")
+#pragma comment(lib, "sfml-window-d.lib")
+#pragma comment(lib, "opengl32.lib")
+
+#endif //PCH_H
